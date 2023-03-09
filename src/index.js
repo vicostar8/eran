4a75ad8785f26f00c64e48b596c2bd93bc498c37
VicoStar <vladchirca8@gmail.com>
2023-03-09 14:16:23 +0200

    Adding temporary HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from "./reportWebVitals";
 
 // APP import
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 // CSS import
 import "./index.css";
@@ -12,9 +12,11 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
